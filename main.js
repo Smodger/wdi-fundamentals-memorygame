ec9bb08e03693416cfe0f4ff23feb93b60583cbf
Smodger <chrisnorman1988@hotmail.co.uk>
2016-09-15 11:42:06 +0100

    assignment 10 complete

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardOne === cardTwo){
-alert ("You found a match!");
-} else if (cardThree === cardFour){
-alert ("You found a match!");
-} else {
-alert ("Sorry, try again.")
-}
+//if (cardOne === cardTwo){
+//alert ("You found a match!");
+//} else if (cardThree === cardFour){
+//alert ("You found a match!");
+//} else {
+//alert ("Sorry, try again.")
+//}
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i=0; i<c4; i++) {
+
+//not sure if 'document.createElement(div)' is correct. possibly ('.board')??
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+
+    board.appendChild(newCard);
+  }
+
+};
